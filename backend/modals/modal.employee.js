5a3af1218e30543dab7a7215bf5fa4715b048b1c
benayascode <mekashabetel@gmail.com>
2025-06-06 16:25:52 +0300

    add Employee model with schema definition and validation

--- backend/modals/modal.employee.js
+++ backend/modals/modal.employee.js
@@ -1,4 +1,55 @@
 import mongoose from "mongoose";
 
+const employeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "default.jpg",
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    position: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "on leave"],
+      default: "inactive",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  { timestamps: true }
+);
 
-//.....
+const Employee = mongoose.model("Employee", employeeSchema);
+export default Employee;
