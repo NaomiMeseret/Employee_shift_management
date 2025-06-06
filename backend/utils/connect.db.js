c7fe7f925633f87fd92af5b1d4bc2bfa4002f014
dagi97 <dagmawitsisay97@gmail.com>
2025-06-06 16:43:07 +0300

    add connection timeouts and log

--- backend/utils/connect.db.js
+++ backend/utils/connect.db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
- const connectDB = async () => {
+
+const connectDB = async () => {
   try{
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI ,{
+      connectTimeoutMS: 30000, // extend timeout
+  socketTimeoutMS: 45000
+    });
   }catch(err){
     console.log(err)
   } 
-   
+  console.log("MongoDB connected");
 }
 
 export default connectDB;
