3e2379207f1188c6e639d4d3d16c7143802a396f
benayascode <mekashabetel@gmail.com>
2025-06-06 16:29:28 +0300

    create Shift model with attendance subdocument

--- backend/modals/modal.shift.js
+++ backend/modals/modal.shift.js
@@ -1,2 +1,47 @@
 import mongoose from "mongoose";
-// .....
+
+const shiftSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    employeeId: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    shiftType: {
+      type: String,
+      enum: ["morning", "afternoon", "night"],
+      required: true,
+    },
+    attendance: [
+      {
+        actionType: {
+          type: String,
+          enum: ["Clock In", "Clock Out"],
+        },
+        time: {
+          type: String,
+        },
+        date: {
+          type: String,
+        },
+        status: {
+          type: String,
+          enum: ["active", "on leave", "inactive"],
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Shift = mongoose.model("Shift", shiftSchema);
+export default Shift;
