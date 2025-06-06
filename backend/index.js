d54203d79ff3f04aad2de1d78fd7f3742057d703
dagi97 <dagmawitsisay97@gmail.com>
2025-06-06 17:02:38 +0300

    initial server and DB setup with placeholder connectDB handling

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
-
-
+import dotenv from "dotenv";
+import connectDB from "./utils/connect.db.js";
 
 const app = express()
-
-app.listen(3000)
+dotenv.config();
+if (process.env.NODE_ENV !== "test") {
+      
+        const port = process.env.PORT || 3000;
+        app.listen(port, connectDB());
+  
+  }
