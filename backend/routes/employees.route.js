560a10161ed1df2abbcec4eb13558ef9e8bd213e
rahel yabebal <rahelyabebal@gmail.com>
2025-06-06 17:52:37 +0300

    Add authentication and employee management routes

--- backend/routes/employees.route.js
+++ backend/routes/employees.route.js
@@ -1,7 +1,38 @@
 // routes/employee.routes.js
 import express from "express";
+import {
+  register,
+  login,
+  logout,
+  changePassword,
+  getAllEmployees,
+  getOneEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from "./routes.js";
 
+const router = express.Router();
 
+// Register
+router.post("/register", register); //works
 
+// Login
+router.post("/login", login); //works
 
-//....
+// Logout
+router.post("/logout", logout); //works
+
+// Change password
+router.post("/changePassword/:id", changePassword); // works
+
+// Get all employees
+router.get("/employees", getAllEmployees); //works
+
+// Get one employee
+router.get("/employees/:id", getOneEmployee); //works
+
+// Update employee
+router.put("/updateEmployee/:id", updateEmployee); //works
+
+// Delete employee
+router.delete("/deleteEmployee/:id", deleteEmployee); //works
