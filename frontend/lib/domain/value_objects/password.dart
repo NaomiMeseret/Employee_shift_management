d19277eaae225c8c52f9076f686b010c8c45361c
benayascode <mekashabetel@gmail.com>
2025-06-07 19:19:36 +0300

    implement Email and Password classes with format and length validation

--- frontend/lib/domain/value_objects/password.dart
+++ frontend/lib/domain/value_objects/password.dart
@@ -1,4 +1,8 @@
 class Password {
- 
- 
+  final String value;
+  Password(this.value) {
+    if (value.length < 6) {
+      throw FormatException('Password must be at least 6 characters');
+    }
+  }
 } 
