db7dd26052e91879719c2046a36b95615a34cffa
benayascode <mekashabetel@gmail.com>
2025-06-07 19:31:15 +0300

    add EmployeeHomeScreen with basic welcome UI

---
+++ frontend/lib/presentation/screens/employee_home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EmployeeHomeScreen extends StatelessWidget {
+  const EmployeeHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Employee Home')),
+      body: const Center(child: Text('Welcome, Employee!')),
+    );
+  }
+} 
