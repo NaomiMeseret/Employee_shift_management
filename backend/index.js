dbf257dbc663f94a49bf9fe990e6dabf6345aa37
benayascode <mekashabetel@gmail.com>
2025-06-07 18:38:39 +0300

    Refactor server setup: add middleware, CORS, and employee routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,32 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./utils/connect.db.js";
+import employeeRoutes from "./routes/employees.route.js";
+import cors from 'cors';
 
-const app = express()
+
 dotenv.config();
-if (process.env.NODE_ENV !== "test") {
+const app = express();
+
+// Middleware to log all incoming API requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${res.stats} ${200 <= res.statusCode < 300  ? '✅' : '❌'}}`);
+    next();
+  });
+  
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  
+  app.use("/api", employeeRoutes);
+
+  
+
+  if (process.env.NODE_ENV !== "test") {
       
         const port = process.env.PORT || 3000;
         app.listen(port, connectDB());
   
   }
+  
+  export default app;
